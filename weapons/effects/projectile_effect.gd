7530cd117ab4d4b10b3d7f439a8fa83c8d4cd352
aramrw <106574385+aramrw@users.noreply.github.com>
2026-04-04 14:27:57 -0700

    Update ProjectileEffect to use new setup method and pass firer node

--- weapons/effects/projectile_effect.gd
+++ weapons/effects/projectile_effect.gd
@@ -6,21 +6,20 @@ class_name ProjectileEffect
 @export var speed: float = 30.0
 @export var damage: int = 25
 
-func execute(_source_node: Node, weapon_manager: Node) -> void:
+func execute(source_node: Node, weapon_manager: Node) -> void:
 	if not projectile_scene: return
 	
 	var proj = projectile_scene.instantiate()
-	# Add to the scene tree root or a dedicated projectiles node
+	# Add to tree
 	weapon_manager.get_tree().root.add_child(proj)
 	
 	# Position at the aim raycast
 	var ray = weapon_manager.aim_raycast
 	proj.global_transform = ray.global_transform
 	
-	# Set properties if the projectile script supports them
+	# Use the new setup method
 	if proj.has_method("setup"):
-		proj.setup(damage, speed, -ray.global_transform.basis.z)
-	elif "speed" in proj:
-		proj.speed = speed
-		proj.damage = damage
-		proj.direction = -ray.global_transform.basis.z
+		var dir = -ray.global_transform.basis.z
+		# firer is the player's body usually, or the weapon_manager's parent
+		var firer = weapon_manager.get_parent() 
+		proj.setup(firer, dir, damage, speed)
