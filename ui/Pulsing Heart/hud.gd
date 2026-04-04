87fb75cb5339647f3f9869a7ac5ebf459abad7f6
aramrw <106574385+aramrw@users.noreply.github.com>
2026-04-04 07:58:39 -0700

    chore: run linter and optimize process_step nesting

--- ui/Pulsing Heart/hud.gd
+++ ui/Pulsing Heart/hud.gd
@@ -18,7 +18,7 @@ func _process(_delta):
 	fps_label.text = str(Engine.get_frames_per_second())
 
 func update_weapon_ui(weapon: WeaponData):
-	if weapon.hud_hint == "SHELLS" or weapon.hud_hint == "BULLETS":
+1	if weapon.hud_hint == "SHELLS" or weapon.hud_hint == "BULLETS":
 		bullets.show()
 		magazines.show()
 	else:
